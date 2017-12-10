4c08a05ec9cc67fe74fecc17b1cb401327684f20
david <home45006@126.com>
2017-12-10 21:03:05 +0800

    竞猜模块dubbo接口开发

---
+++ src/main/java/com/fh/rpc/LotteryWebServiceImpl.java
@@ -0,0 +1,32 @@
+package com.fh.rpc;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.iot.game.pooh.web.rpc.interfaces.LotteryWebRpcService;
+
+@Service
+public class LotteryWebServiceImpl implements LotteryWebRpcService {
+
+	/**
+	 * 开始竞猜
+	 */
+	@Override
+	public String startLottery(String roomId) {
+		return null;
+	}
+
+	/**
+	 * 结束竞猜
+	 */
+	@Override
+	public String endLottery(String roomId) {
+		return null;
+	}
+
+	/**
+	 * 开始算奖
+	 */
+	@Override
+	public String drawLottery(String roomId) {
+		return null;
+	}
+}
