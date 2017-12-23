3fe4c8b19e82c486f313a8ef31e1d22c344d3bf5
zhuqiuyou <zhuqiuyou_zqy@163.com>
2017-12-23 21:57:51 +0800

    srs 定时任务

---
+++ src/main/java/com/fh/job/SrsCameraBizJob.java
@@ -0,0 +1,19 @@
+package com.fh.job;
+
+/**
+ *srs服务器 摄像头推流状态 Job
+ *
+ *@author zhuqiuyou
+ */
+public class SrsCameraBizJob{
+ 
+	
+	/**
+	 * 定时任务同步方法
+	 * @throws Exception
+	 */
+	public void getCameraStateJob() throws Exception{
+		System.out.println("-----------------------------------");
+	}
+
+}
