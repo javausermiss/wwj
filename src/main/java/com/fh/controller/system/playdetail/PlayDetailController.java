f0fba49fac43de67a9e5374c33561f4bf8fa0023
guo <1239449356@qq.com>
2018-01-03 17:44:18 +0800

    commit

---
+++ src/main/java/com/fh/controller/system/playdetail/PlayDetailController.java
@@ -0,0 +1,181 @@
+package com.fh.controller.system.playdetail;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Resource;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.fh.controller.base.BaseController;
+import com.fh.entity.Page;
+import com.fh.service.system.appuser.AppuserManager;
+import com.fh.service.system.doll.DollManager;
+import com.fh.service.system.playdetail.PlayDetailManage;
+import com.fh.util.ObjectExcelView;
+import com.fh.util.PageData;
+import com.fh.util.Jurisdiction;
+
+/** 
+ * 说明：抓取记录
+ * 创建人：FH Q313596790
+ * 创建时间：2017-12-29
+ */
+@Controller
+@RequestMapping(value="/playdetail")
+public class PlayDetailController extends BaseController {
+	
+	String menuUrl = "playdetail/list.do"; //菜单地址(权限用)
+	@Resource(name="playDetailService")
+	private PlayDetailManage playdetailservice;
+	
+	@Resource(name="dollService")
+	private DollManager dollService;
+	
+	@Resource(name="appuserService")
+	private AppuserManager appuserservice;
+	/**保存
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/save")
+	public ModelAndView save() throws Exception{
+		logBefore(logger, Jurisdiction.getUsername()+"新增PlayDetail");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "add")){return null;} //校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		pd.put("ID", this.get32UUID());	//主键
+		playdetailservice.save(pd);
+		mv.addObject("msg","success");
+		mv.setViewName("save_result");
+		return mv;
+	}
+	
+	
+	/**修改
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/edit")
+	public ModelAndView edit() throws Exception{
+		logBefore(logger, Jurisdiction.getUsername()+"修改PlayDetail");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "edit")){return null;} //校验权限
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		playdetailservice.edit(pd);
+		mv.addObject("msg","success");
+		mv.setViewName("save_result");
+		return mv;
+	}
+	
+	/**列表
+	 * @param page
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/list")
+	public ModelAndView list(Page page) throws Exception{
+		logBefore(logger, Jurisdiction.getUsername()+"列表PlayDetail");
+		//if(!Jurisdiction.buttonJurisdiction(menuUrl, "cha")){return null;} //校验权限(无权查看时页面会有提示,如果不注释掉这句代码就无法进入列表页面,所以根据情况是否加入本句代码)
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		String keywords = pd.getString("keywords");				//关键词检索条件
+		if(null != keywords && !"".equals(keywords)){
+			pd.put("keywords", keywords.trim());
+		}
+		page.setPd(pd);
+		List<PageData>	varList = playdetailservice.list(page);	//列出PlayDetail列表
+		mv.setViewName("system/playdetail/playdetail_list");
+		mv.addObject("varList", varList);
+		mv.addObject("pd", pd);
+		mv.addObject("QX",Jurisdiction.getHC());	//按钮权限
+		return mv;
+	}
+	
+	/**去新增页面
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/goAdd")
+	public ModelAndView goAdd()throws Exception{
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		mv.setViewName("system/playdetail/playdetail_edit");
+		
+		List<PageData>	dolList =dollService.listAll(new PageData());
+		
+		mv.addObject("msg", "save");
+		mv.addObject("pd", pd);
+		mv.addObject("dolList", dolList);
+		return mv;
+	}	
+	
+	 /**去修改页面
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/goEdit")
+	public ModelAndView goEdit()throws Exception{
+		ModelAndView mv = this.getModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		pd = playdetailservice.findById(pd);	//根据ID读取
+		List<PageData>	dolList =dollService.listAll(new PageData());
+		mv.setViewName("system/playdetail/playdetail_edit");
+		mv.addObject("msg", "edit");
+		mv.addObject("pd", pd);
+		mv.addObject("dolList", dolList);
+		return mv;
+	}	
+	
+	
+	 /**导出到excel
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/excel")
+	public ModelAndView exportExcel() throws Exception{
+		logBefore(logger, Jurisdiction.getUsername()+"导出PlayDetail到excel");
+		if(!Jurisdiction.buttonJurisdiction(menuUrl, "cha")){return null;}
+		ModelAndView mv = new ModelAndView();
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		Map<String,Object> dataMap = new HashMap<String,Object>();
+		List<String> titles = new ArrayList<String>();
+		titles.add("用户名");	//1
+		titles.add("娃娃机名称");	//2
+		titles.add("娃娃名称");	//3
+		titles.add("状态");	//4
+		dataMap.put("titles", titles);
+		List<PageData> varOList = playdetailservice.listAll(pd);
+		List<PageData> varList = new ArrayList<PageData>();
+		for(int i=0;i<varOList.size();i++){
+			PageData vpd = new PageData();
+			vpd.put("var1", varOList.get(i).getString("USERID"));	    //1
+			vpd.put("var2", varOList.get(i).getString("DOLLID"));	    //2
+			vpd.put("var3", varOList.get(i).getString("DOLL_NAME"));	    //3
+			vpd.put("var4", varOList.get(i).getString("STATE"));	    //4
+			varList.add(vpd);
+		}
+		dataMap.put("varList", varList);
+		ObjectExcelView erv = new ObjectExcelView();
+		mv = new ModelAndView(erv,dataMap);
+		return mv;
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(format,true));
+	}
+}
