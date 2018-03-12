c6499bfde815639341836b088b9e9a00363f7733
guo <1239449356@qq.com>
2018-03-12 16:59:08 +0800

    查询渠道推广接口

--- src/main/java/com/fh/controller/wwjapp/AppPromoteManageController.java
+++ src/main/java/com/fh/controller/wwjapp/AppPromoteManageController.java
@@ -5,18 +5,19 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.alibaba.dubbo.common.json.JSONObject;
 import com.fh.controller.base.BaseController;
-import com.fh.entity.Page;
 import com.fh.service.system.promotemanage.PromoteManageManager;
 import com.fh.util.Jurisdiction;
 import com.fh.util.PageData;
+import com.fh.util.wwjUtil.RespStatus;
+
+import net.sf.json.JSONObject;
 /**
  * 	查询渠道推广
  *
@@ -28,21 +29,25 @@ public class AppPromoteManageController extends BaseController {
 	@Resource(name="promotemanageService")
 	private PromoteManageManager promotemanageService;
 	
-	@RequestMapping("/getpromomoteManage")
-	@ResponseBody
-	public Map<String,Object> list(){
+
+	/**
+	 * 查询渠道推广
+	 * @param request
+	 * @return
+	 */
+    @RequestMapping(value = "/getpromomoteManage", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public JSONObject getpromomoteManage(HttpServletRequest request) {
 		logBefore(logger, Jurisdiction.getUsername()+"所有数据promote");
-		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("code",0);
-		map.put("msg","success");
+	
 		try{
 			List<PageData>	varList = promotemanageService.listAll();	//列出PromoteManage列表
-			map.put("data", varList);
+			Map<String,Object> dataMap = new HashMap<String,Object>();
+			dataMap.put("varList", varList);
+			return RespStatus.successs().element("data", dataMap);
 		}catch (Exception e) {
-			e.printStackTrace();	
-			map.put("code",-1);
-			map.put("msg","fail");
+			logger.info(e.getMessage());
+			return RespStatus.fail();
 		}
-		return map;
 	}
 }
